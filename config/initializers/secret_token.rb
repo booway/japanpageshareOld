97df90eaa81bd99e20ffe7e5cc2c9c89ae046ffe
Booway Mendjargal <bluespot.b@gmail.com>
2012-01-11 16:07:37 +0800

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Japanpageshare::Application.config.secret_token = 'a822fc0a8cb9fbf708159299adde3676711d5240ce92f04008eb8b4abbf2604ecb039f382791375d27ff23cc088fed116e8691ff1ad3d5eb80da7d64e6414e75'
